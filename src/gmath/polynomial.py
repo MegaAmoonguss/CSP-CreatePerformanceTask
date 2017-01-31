16af9d2f6cb450a0c63aaf427b64328400dafefc
Graham Preston <graham.preston@gmail.com>
2017-01-31 09:59:49 -0500

    Added __str__() to polynomial class

--- src/gmath/polynomial.py
+++ src/gmath/polynomial.py
@@ -4,17 +4,47 @@ class Polynomial:
     """
     
     def __init__(self, coeffs):
-        self.coeffs = coeffs
-        self.order = len(coeffs)
+        """
+        Initialize the Polynomial object.
+        self.coeffs to be the coeffs parameter stripped of leading zeros.
+        self.degree is the degree of the polynomial.
+        
+        Example:
+        p = Polynomial([5, -5, 360])       # Equivalent to 5x^2 - 5x + 360
+        p = Polynomial([0, 0, 5, 1, 0])    # Equivalent to 5x^2 + x
+        """
+        for i in range(len(coeffs)):
+            if coeffs[i] != 0:
+                self.coeffs = coeffs[i:]
+                break
+        
+        self.degree = len(self.coeffs)
         
     def __str__(self):
+        """
+        Return the object as a properly formatted polynomial function.
+        
+        Example:
+        p = Polynomial([5, -5, 360])
+        str(p)                             # 5x^2 - 5x + 360
+        
+        p = Polynomial([0, 0, 5, 1, 0])
+        str(p)                             # 5x^2 + x
+        """
         s = ""
-        for i in reversed(range(self.order)):
-            s += str(self.coeffs[-(i+1)])
+        for i in reversed(range(self.degree)):
+            if i < self.degree - 1:
+                if self.coeffs[-(i+1)] < 0:
+                    s += " - "
+                elif self.coeffs[-(i+1)] > 0:
+                    s += " + "
             
-            if i > 0:
-                s += "x"
-                if i > 1:
-                    s += "^" + str(i)
-            s += " + "
-        return s[:-3]
+            if self.coeffs[-(i+1)] != 0:
+                if self.coeffs[-(i+1)] != 1:
+                    s += str(abs(self.coeffs[-(i+1)]))
+                if i > 0:
+                    s += "x"
+                    if i > 1:
+                        s += "^" + str(i)
+            
+        return s
