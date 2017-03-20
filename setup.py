8acad22da067ef7505584396972d6d564c4604b8
Graham Preston <graham.preston@gmail.com>
2017-03-19 21:29:24 -0400

    Change name to gmath

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name="GMath-py",
+setup(name="GMath",
       version="0.0.1",
       description="A Python implementation of the depricated GMath Java library",
       long_description="Useful calculation functions in Python, such as quadratic factoring, repeating decimal notation, decimal to fraction, and more.",
@@ -11,5 +11,5 @@ setup(name="GMath-py",
       install_requires=["click"],
       entry_points="""
           [console_scripts]
-          gmath-py=commands:cli
+          gmath=commands:cli
       """)
