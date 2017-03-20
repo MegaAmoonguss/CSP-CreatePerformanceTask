d02637ac18b16eb15e65617d320e29e0a685a113
Graham Preston <graham.preston@gmail.com>
2017-03-19 22:35:39 -0400

    Remove useless long description

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 
 setup(name="gmath",
       description="A practical mini math library/CLI",
-      long_description="Useful calculation functions in Python, such as quadratic factoring, repeating decimal notation, decimal to fraction, and more.",
       url="https://github.com/MegaAmoonguss/gmath",
       author="Graham Preston",
       author_email="graham.preston@gmail.com",
